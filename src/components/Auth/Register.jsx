2e9fe295350a516ac4aefc059e2b361f565109ab
ibrahim2102 <mdibrahim91695@gmail.com>
2025-11-14 15:42:55 +0600

    done from registration

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -13,7 +13,6 @@ const Register = () => {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
 
-
     const handleGoogleSignIn = () => {
         signInWithGoogle()
             .then(result => {
